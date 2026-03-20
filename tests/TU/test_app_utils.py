ea7b51af54b97cb901f8d6d7088d39908ce1b7a5
Miltiade <vero@tutamail.com>
2026-03-20 08:06:22 +0200

    debugged

--- tests/TU/test_app_utils.py
+++ tests/TU/test_app_utils.py
@@ -1,5 +1,8 @@
 import pytest
 from app_utils import find_club_by_email
+import sys
+import os
+sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), '../../'))) # Ajoute le dossier racine au début des chemins de recherche modules
 
 def test_find_club_par_email_valide():
     club = find_club_by_email("john@simplylift.co")
