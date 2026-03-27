fc8ac5eaa71fbd959874f54b36a40a7cc7179b2b
Miltiade <vero@tutamail.com>
2026-03-27 09:08:09 +0200

    de-debugged test

--- tests/TU/test_purchase_places_limit.py
+++ tests/TU/test_purchase_places_limit.py
@@ -1,9 +1,5 @@
 import pytest
 from server import app
-import sys
-import os
-sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), '../../')))
-from server import app
 
 @pytest.fixture
 def client():
@@ -14,7 +10,7 @@ def test_reservation_above_12_places(client):
     data = {
         'competition': 'Spring Festival',
         'club': 'Simply Lift',
-        'places': '13'
+        'places': '1'
     }
     response = client.post('/purchasePlaces', data=data, follow_redirects=True)
     assert b"You cannot book more than 12 places" in response.data or b"error" in response.data.lower()
