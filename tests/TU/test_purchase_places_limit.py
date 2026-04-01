3e7e00e8a9753b4ac04c051d63e8e9b8fe9ee9c4
Miltiade <vero@tutamail.com>
2026-04-01 08:27:14 +0300

    created "happy path" unit test

--- tests/TU/test_purchase_places_limit.py
+++ tests/TU/test_purchase_places_limit.py
@@ -14,3 +14,12 @@ def test_reservation_above_12_places(client):
     }
     response = client.post('/purchasePlaces', data=data, follow_redirects=True)
     assert b"You cannot book more than 12 places" in response.data or b"error" in response.data.lower()
+
+def test_reservation_up_to_12_places(client):
+    data = {
+        'competition': 'Spring Festival',
+        'club': 'Simply Lift',
+        'places': '12'
+    }
+    response = client.post('/purchasePlaces', data=data, follow_redirects=True)
+    assert b"Great-booking complete!" in response.data
