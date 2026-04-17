bdb0687a299beb1a32165bef43183a8e768a77ed
Miltiade <vero@tutamail.com>
2026-04-17 09:16:50 +0300

    debugged: added "update club's point balance" feature

--- server.py
+++ server.py
@@ -47,7 +47,7 @@ def purchasePlaces():
     club = [c for c in clubs if c['name'] == request.form['club']][0]
     placesRequired = int(request.form['places'])
     competition['numberOfPlaces'] = int(competition['numberOfPlaces'])-placesRequired
-    # club['points'] = int(club['points']) - placesRequired # Update club's points balance
+    club['points'] = int(club['points']) - placesRequired # Update club's points balance
     flash('Great-booking complete!')
     return render_template('welcome.html', club=club, competitions=competitions)
 
