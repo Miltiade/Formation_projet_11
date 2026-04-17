a9454d489e20af77d4102b94a484470b7f6b54b2
Miltiade <vero@tutamail.com>
2026-04-17 08:33:58 +0300

    created UT test "sad path"

---
+++ tests/TU/test_booking_good_competitions.py
@@ -0,0 +1,9 @@
+def test_booking_future_competition(client):
+    data = {
+        'competition': 'Winter Wonderland',  # compétition dans le futur ou récente selon vos données
+        'club': 'Simply Lift',
+        'places': '2'
+    }
+    response = client.post('/purchasePlaces', data=data, follow_redirects=True)
+    # On vérifie que le message de succès est affiché
+    assert b"Great-booking complete!" in response.data
